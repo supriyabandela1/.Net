f0dd592ef0b577671f9598feadb4e5ad1bd29ca2
Supriya <83172432+supriyabandela1@users.noreply.github.com>
2022-04-15 22:33:56 -0400

    Add files via upload
    Creating Question component

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {QuestionComponent} from './question.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
